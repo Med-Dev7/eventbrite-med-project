0ae6abbf0c0918fef13bd9ed83292f99357fbe10
mehdi <mourzagh.m@live.fr>
2019-11-04 20:32:21 +0100

    add mailer2

--- Eventbrite/db/seeds.rb
+++ Eventbrite/db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts "u start"
+u = User.create(
+    first_name: "azerty",
+    last_name: "Clavier",
+    description: "ceci est un essaie",
+    email: "azerty-thp@yopmail.com",   
+    encrypted_password: "try" )
+    #email: "mourzagh.m@live.fr",
+    puts "u end"
+puts "e start"
+# time = Time.now + 3600
+# e = Event.new(
+#     start_date: Time.now,
+# )
+e = Event.create(
+    start_date: Time.now,
+    duration: 30,
+    title: "Try and succees",
+    description: "try and try and try and try and try",
+    price: 16,
+    location: "yes"
+)
+puts "e end"
+puts "a start"
+a = Attendance.create(user_id: u.id, event_id: e.id)
+puts "a end"
+
+
